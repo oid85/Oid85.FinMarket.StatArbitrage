be3446bb9dfeeb87fc38a47f0892f1df5b4b5484
oid85 <oid85@mail.ru>
2026-08-01 22:00:11 +0700

    .

---
+++ Oid85.PrintTemplate/Oid85.PrintTemplate.Core/Requests/CreatePrintTemplateRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Oid85.PrintTemplate.Core.Requests
+{
+    public class CreatePrintTemplateRequest
+    {
+        public ResponseDataModel Data { get; set; } = new();
+    }
+
+    public class ResponseDataModel
+    {
+
+    }
+}
